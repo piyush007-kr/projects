2860f47b94c4afa0bbc224e7910fb04125dbaae8
PIYUSH <psah06348@gmail.com>
2026-01-13 12:48:03 +0530

    Update SQUARE.py

--- SQUARE.py
+++ SQUARE.py
@@ -1,5 +1,6 @@
-# code for making square by using star 
+# code for making square by using asterik
 a=int(input("Enter the width you want to make square>>>> "))
 for i in range(0,a):
     b=" * "
     print(a*b)
+
